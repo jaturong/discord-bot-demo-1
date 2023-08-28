8acd7b4a7264c82f130552884b909ff691f19a38
jaturong <thonsunan@gmail.com>
2023-08-29 00:02:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const dotenv = require('dotenv');
+dotenv.config();
+
+const color = ["Red", "Green", "Blue"]
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+
+  if (interaction.commandName === 'color') {
+    await interaction.reply(color[Math.floor(Math.random() * color.length)]);
+  }
+});
+
+client.login(process.env.TOKEN);
---
+++ register.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    {
+        name: 'color',
+        description: 'Random-Color',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN); 
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+    
+        await rest.put(Routes.applicationCommands(process.env.ClientID, process.env.ChannelID), { body: commands });
+    
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
